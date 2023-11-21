8b87c2e1085322657bc42187831fd290584f0ce2
Hansimov <591172499@qq.com>
2023-11-21 11:18:45 +0800

    :recycle: [Refactor] Seperate ConversationConnectRequestHeadersConstructor

--- conversation_connector.py
+++ conversation_connector.py
@@ -13,6 +13,26 @@ from logger.logger import logger
 http_proxy = "http://localhost:11111"  # Replace with yours
 
 
+class ConversationConnectRequestHeadersConstructor:
+    def __init__(self):
+        self.construct()
+
+    def construct(self):
+        self.request_headers = {
+            "Accept-Encoding": " gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+            "Cache-Control": "no-cache",
+            "Connection": "Upgrade",
+            "Host": "sydney.bing.com",
+            "Origin": "https://www.bing.com",
+            "Pragma": "no-cache",
+            "Sec-Websocket-Extensions": "permessage-deflate; client_max_window_bits",
+            "Sec-Websocket-Version": "13",
+            "Upgrade": "websocket",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
+        }
+
+
 class ConversationConnector:
     def __init__(
         self,
@@ -43,22 +63,10 @@ class ConversationConnector:
 
     async def stream_chat(self, prompt=""):
         self.aiohttp_session = aiohttp.ClientSession(cookies=self.cookies)
-        request_headers = {
-            "Accept-Encoding": " gzip, deflate, br",
-            "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
-            "Cache-Control": "no-cache",
-            "Connection": "Upgrade",
-            "Host": "sydney.bing.com",
-            "Origin": "https://www.bing.com",
-            "Pragma": "no-cache",
-            "Sec-Websocket-Extensions": "permessage-deflate; client_max_window_bits",
-            "Sec-Websocket-Version": "13",
-            "Upgrade": "websocket",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
-        }
+        request_headers_constructor = ConversationConnectRequestHeadersConstructor()
         self.wss = await self.aiohttp_session.ws_connect(
             self.ws_url,
-            headers=request_headers,
+            headers=request_headers_constructor.request_headers,
             proxy=http_proxy,
         )
 
@@ -70,7 +78,6 @@ class ConversationConnector:
             conversation_id=self.conversation_id,
             invocation_id=self.invocation_id,
         )
-        chathub_request_constructor.construct()
 
         await self.wss_send(chathub_request_constructor.request_payload)
 
